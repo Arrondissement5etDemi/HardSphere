2405635dbb9765272c328c14a6280df5034d4719
Haina <43018326+Studio-Darboux-Carbonnier@users.noreply.github.com>
2019-01-15 23:02:49 +0000

    Add files via upload

--- HardSphereSmall.java
+++ HardSphereSmall.java
@@ -7,28 +7,40 @@ public class HardSphereSmall {
 		int n = 500;
 		double diam = 0.0914156;//for phi = 0.20
 		double[][] coordinates = cubeLattice(8,1.0);
-		double thickness = 0.0084;
+		double thickness = diam/10.0;
                 System.out.println("thickness = " + thickness);
                 System.out.println("diam = " + diam);
 
 		Box pandora3D = new Box(n,diam,coordinates);
 		//int collisionNow = pandora3D.totalCollision();		
 
-		for (int i = 1; i <= 20000000; i++) {
+		double[][] g2Final = RadialStat.g2Table(pandora3D,thickness);
+		for (int i = 0; i < g2Final.length; i++) {
+			g2Final[i][1] = 0.0;
+		}
+		int relaxTime =  100000;
+		int totalTime = 5000000;
+		int numSample = totalTime/relaxTime;
+
+		for (int i = 1; i <= totalTime; i++) {
 			Movement m = pandora3D.move(0.005);
 			Particle particleMoved = m.getParticle();
 			if (pandora3D.collisionChecker(particleMoved,n) > 1) {
 				pandora3D.move(m.reverse());
 			}
-			if (i%1000000 == 0) {//get the config and g2 every 1000000 moves	 
-				System.out.println(pandora3D);//this is the equil. config.
+			if (i%relaxTime == 0) {//sample the config and g2 after every relaxation
+				//System.out.println(pandora3D);//this is the equil. config.
                 		double[][] g2 = RadialStat.g2Table(pandora3D,thickness);
-                		for (int j = 0; j < g2.length; j++) {
-                        		System.out.println(g2[j][0] + " " + g2[j][1]);
-                		}
+				for (int j = 0; j < g2.length; j++) {
+					g2Final[j][1] = g2Final[j][1] + g2[j][1];
+				}
 			}
 		}
-	
+
+		for (int i = 0; i < g2Final.length; i++) {
+			g2Final[i][1] = g2Final[i][1]/(double)numSample;
+			System.out.println(g2Final[i][0] + " " + g2Final[i][1]);
+		}
 	}	
 
 	/**generates the coordinates of a square lattice in 3D 
